970b161ed8140360e8a978c27e712e6b86a1ff83
Tobias Elod-Zoltan <etobias@pentalog.fr>
2015-10-05 17:51:50 +0300

    [DEV] Base commit
    - added base files
    - proposal image uploaded
    
    Signed-off-by: Tobias Elod-Zoltan <etobias@pentalog.fr>

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,15 @@ module.exports = function gruntInit(grunt) {
     grunt.initConfig({
         eslint: {target: ['src/index.js']},
         flow: {files: {}},
-        browserSync: {dev: {options: {proxy: 'localhost:3000'}}}
+        browserSync: {dev: {options: {proxy: 'localhost:3000'}}},
+        copy: {build: {files: [{src: ['src/index.js'], dest: 'build/'}]}}
     });
 
     grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-flow');
 
-    grunt.registerTask('develope', ['eslint', 'flow', 'browserSync']);
+    grunt.registerTask('build', ['eslint', 'flow', 'copy']);
+    grunt.registerTask('develope', ['build', 'browserSync']);
 };
